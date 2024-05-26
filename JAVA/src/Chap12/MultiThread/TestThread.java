34a09eca9c2f089beab678d4e3cbd49523452198
kjw <stlee0926@naver.com>
2024-05-26 20:35:37 +0900

    폴더 최종 정리

--- JAVA/src/Chap12/MultiThread/TestThread.java
+++ JAVA/src/Chap12/MultiThread/TestThread.java
@@ -1,4 +1,11 @@
 package Chap12.MultiThread;
 
 public class TestThread {
+
+    public static void main(String[] args) {
+        System.out.println("실행");
+
+
+        System.out.println("종료");
+    }
 }
