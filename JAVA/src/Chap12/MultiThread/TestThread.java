2c27a12782bd902c4eb39e3d988d2b4c1c9686cd
kjw <stlee0926@naver.com>
2024-05-26 20:49:25 +0900

    멀티 스레드 일부 공부

--- JAVA/src/Chap12/MultiThread/TestThread.java
+++ JAVA/src/Chap12/MultiThread/TestThread.java
@@ -5,7 +5,34 @@ public class TestThread {
     public static void main(String[] args) {
         System.out.println("실행");
 
+        Thread currThread = Thread.currentThread();//Instance 생성 없이 사용 가능한 정적 메소드
+
+        System.out.println(currThread.getName());//Thread는 이름이 있음.
+
+        Thread1 thread1 = new Thread1();
+        Thread thread2 = new Thread(new Thread2());
+
+        thread1.start();
+        thread2.start();
+
 
         System.out.println("종료");
+    }
+}
+
+
+//아래 두 방법 다 결국 run이라는 함수를 채워주는 것은 동일하다.
+class Thread1 extends Thread{ //Thread 클래스를 상속 받아 구현하는 방법
+    @Override
+    public void run(){
+        System.out.println(currentThread().getName());
+    }
+}
+
+class Thread2 implements Runnable{ //Runnable 인터페이스를 구현 받아 구현하는 방법
+    @Override
+    public void run() {
+        Thread currThread = Thread.currentThread();//Instance 생성 없이 사용 가능한 정적 메소드
+        System.out.println(currThread.getName());
     }
 }
