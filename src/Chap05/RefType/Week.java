e21eb4be5f4ae70094dd5d27c92e23a2d25dbb23
kjw <stlee0926@naver.com>
2024-05-26 18:38:41 +0900

    Enum Study

---
+++ src/Chap05/RefType/Week.java
@@ -0,0 +1,11 @@
+package Chap05.RefType;
+
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
--- src/Lab01/Thread/TestThread.java
+++
@@ -1,2 +0,0 @@
-package Lab01.Thread;public class TestThread {
-}
